f67655c937bcd3eb69e8ab0af0ee641499fe96f3
keepers-of-the-night <120640608+keepers-of-the-night@users.noreply.github.com>
2023-03-30 19:53:54 +0300

    Add files via upload

---
+++ TehProg7/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.Формула_3;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 39);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(129, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "5.5",
+            "4.2",
+            "16.4",
+            "7.1"});
+            this.listBox1.Location = new System.Drawing.Point(13, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(30, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "0.8",
+            "4.3",
+            "7.7",
+            "61.2"});
+            this.listBox2.Location = new System.Drawing.Point(68, 105);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(30, 95);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(75, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ TehProg7/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string st = "C:/Student/16";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<double> x = new List<double>() {5.5, 4.2, 16.4, 7.1}, y = new List<double>() { 0.8, 4.3, 7.7, 61.2 };
+            /*for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                x.Add(Convert.ToDouble(listBox1.Items[i]));
+            }
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                y.Add(Convert.ToDouble(listBox2.Items[i]));
+            }*/
+            if (x.Count != y.Count)
+                MessageBox.Show("Количество иксов не соответствует количеству игриков!");
+            else
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    double d = Function(x[i], y[i]);
+                        rand(d);
+                }
+            }
+        }
+        private double Function(double x, double y){
+            return Math.Pow(x*x*x*x*x-2, 1/3)/y;
+        }
+        private void rand( double d)
+        {
+            //Создание объекта для генерации чисел
+            Random rnd = new Random();
+ 
+            //Получить очередное (в данном случае - первое) случайное число
+            int value = rnd.Next(4, 4);
+            switch (value)
+            {
+                case 1:
+                    BinFile1(d);
+                    break;
+                case 2:
+                    BinFile2(d);
+                    break;
+                case 3:
+                    BinFile3(d);
+                    break;
+                case 4:
+                    BinFile4(d);
+                    break;
+            }
+        }
+        private void BinFile1(double d)
+        {
+            string s = st + "/G0001.rez";
+            using (BinaryWriter b = new BinaryWriter(File.Open(s, FileMode.OpenOrCreate))) {
+                b.Write(Convert.ToString(d));
+            }
+            using (BinaryReader b = new BinaryReader(File.Open(s, FileMode.Open)))
+                        {
+                            int pos = 50000;
+                            int required = 2000;
+
+                            // Seek the required index.
+                            b.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+                            // Read the next 2000 bytes.
+                            byte[] by = b.ReadBytes(required);
+                        }
+            
+
+        }
+        private void BinFile2(double d)
+        {
+            string s = st + "/G0002.rez";
+            using (BinaryWriter b = new BinaryWriter(File.Open(s, FileMode.OpenOrCreate)))
+            {
+                b.Write(Convert.ToString(d));
+            }
+            using (BinaryReader b = new BinaryReader(File.Open(s, FileMode.Open)))
+            {
+                int pos = 50000;
+                int required = 2000;
+
+                // Seek the required index.
+                b.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+                // Read the next 2000 bytes.
+                byte[] by = b.ReadBytes(required);
+            }
+
+        }
+        private void BinFile3(double d)
+        {
+            string s = st + "/G0003.rez";
+            using (BinaryWriter b = new BinaryWriter(File.Open(s, FileMode.OpenOrCreate)))
+            {
+                b.Write(Convert.ToString(d));
+            }
+            using (BinaryReader b = new BinaryReader(File.Open(s, FileMode.Open)))
+            {
+                int pos = 50000;
+                int required = 2000;
+
+                // Seek the required index.
+                b.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+                // Read the next 2000 bytes.
+                byte[] by = b.ReadBytes(required);
+            }
+
+        }
+        private void BinFile4(double d)
+        {
+            string s = st + "/G0004.rez";
+            using (BinaryWriter b = new BinaryWriter(File.Open(s, FileMode.OpenOrCreate)))
+            {
+                b.Write(Convert.ToString(d));
+            }
+            using (BinaryReader b = new BinaryReader(File.Open(s, FileMode.Open)))
+            {
+                int pos = 0;
+                int required = 2000;
+                string strin = "";
+
+                //b.(Convert.ToString(d));
+
+                // Seek the required index.
+                b.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+                // Read the next 2000 bytes.
+                byte[] by = b.ReadBytes(required);
+                MessageBox.Show(string.Join("", by));
+                while (b.PeekChar() > -1)
+                    strin = string.Join("", b.ReadDouble());
+                MessageBox.Show(strin);
+            }
+
+        }
+    }
+}
